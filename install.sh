ffc46e5c7496ef77e0db7bf0bd60bf0d1e44fcce
CoderYCG <1050676515@qq.com>
2017-10-19 10:19:24 +0800

    GoTools

---
+++ install.sh
@@ -0,0 +1,16 @@
+go install github.com/zmb3/gogetdoc
+go install golang.org/x/tools/cmd/guru 
+go install github.com/golang/lint/golint
+go install github.com/davidrjenni/reftools/cmd/fillstruct
+go install github.com/rogpeppe/godef
+go install github.com/fatih/motion
+go install github.com/kisielk/errcheck
+go install github.com/nsf/gocode
+go install github.com/jstemmer/gotags
+go install github.com/josharian/impl
+go install golang.org/x/tools/cmd/goimports
+go install github.com/fatih/gomodifytags
+go install github.com/dominikh/go-tools/cmd/keyify
+go install golang.org/x/tools/cmd/gorename
+go install github.com/klauspost/asmfmt/cmd/asmfmt
+go install github.com/alecthomas/gometalinter
